6644d27b0065f09ab72eb3bbcfcd3bc870ad9bee
basil-ahmed <90772853+basil-ahmed@users.noreply.github.com>
2022-09-29 00:53:13 +0400

    Add files via upload

--- WorldFoodRecipes/script.js
+++ WorldFoodRecipes/script.js
@@ -2,8 +2,10 @@ window.addEventListener('load', function() {
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=') //access the json
     .then(response => response.json())
     .then(data => { // access the data IF the promise is fulfilled
-      console.log(data);
-      for (let index = 0; index < data.meals.length; index++) {
+
+      console.log(data); // access the JSON data
+
+      for (let index = 0; index < data.meals.length; index++) { // Going through the list of Meals and making elements for each to display on the webpage
         let meal = data.meals[index];
         let a = this.document.createElement('a');
         a.href = meal.strYoutube;
@@ -20,8 +22,6 @@ window.addEventListener('load', function() {
         text.style.fontSize = '40px';
         text.style.color = 'white';
         text.style.fontWeight = '900';
-        // text.classList = "purple-text";
-        // text.classList = "web-name";
         let parent = this.document.getElementById('parent');
         parent.appendChild(a);
         a.appendChild(bg);
@@ -31,52 +31,49 @@ window.addEventListener('load', function() {
       const boxes=document.querySelectorAll('.portfolio-item-wrapper')//store all initial elements of class portfolio-item-wrapper
       const searchBar = document.getElementById('searchBar'); //search bar element
 
-      searchBar.addEventListener('keyup',(e)=>{
-          //searches the input and displays relevant boxes
+      searchBar.addEventListener('keyup',(e)=>{ //searches the input and displays relevant boxes
 
-          //remove all boxes currently on screen
-          const currentBoxes=document.querySelectorAll('.portfolio-item-wrapper');
-          currentBoxes.forEach(box1 => {
-              identity=box1.id;
-              removeElement(identity); //removes element from code
-          });
-          //store search input
-          const searchString = e.target.value.toLowerCase();
+        //remove all boxes currently on screen
+        const currentBoxes=document.querySelectorAll('.portfolio-item-wrapper');
+        currentBoxes.forEach(box1 => {
+          identity=box1.id;
+          removeElement(identity); //removes element from code
+        });
 
-          for (let index = 0; index < data.meals.length; index++) {
+        //store search input
+        const searchString = e.target.value.toLowerCase();
 
-            let meal = data.meals[index];
+        for (let index = 0; index < data.meals.length; index++) { // Goes through the loop and only displays the items that contains the search bar input
 
-            if (meal.strMeal.toLowerCase().includes(searchString)) {
-              let a = this.document.createElement('a');
-              a.href = meal.strYoutube;
-              a.classList = "portfolio-item-wrapper";
-              a.id = toString(index);
-              a.target="_blank";
-              let bg = this.document.createElement('div');
-              bg.classList = "portfolio-img-background";
-              let imageURL = meal.strMealThumb;
-              bg.style.backgroundImage = 'url('+imageURL+')';
-              let text = this.document.createElement('div');
-              text.classList = "image-text-wrapper";
-              text.innerHTML = meal.strMeal;
-              text.style.fontSize = '40px';
-              text.style.color = 'white';
-              text.style.fontWeight = '900';
-              // text.classList = "purple-text";
-              // text.classList = "web-name";
-              let parent = this.document.getElementById('parent');
-              parent.appendChild(a);
-              a.appendChild(bg);
-              bg.appendChild(text);  
-            }
-            
+          let meal = data.meals[index];
+
+          if (meal.strMeal.toLowerCase().includes(searchString)) {
+            let a = this.document.createElement('a');
+            a.href = meal.strYoutube;
+            a.classList = "portfolio-item-wrapper";
+            a.id = toString(index);
+            a.target="_blank";
+            let bg = this.document.createElement('div');
+            bg.classList = "portfolio-img-background";
+            let imageURL = meal.strMealThumb;
+            bg.style.backgroundImage = 'url('+imageURL+')';
+            let text = this.document.createElement('div');
+            text.classList = "image-text-wrapper";
+            text.innerHTML = meal.strMeal;
+            text.style.fontSize = '40px';
+            text.style.color = 'white';
+            text.style.fontWeight = '900';
+            let parent = this.document.getElementById('parent');
+            parent.appendChild(a);
+            a.appendChild(bg);
+            bg.appendChild(text);  
           }
+          
+        }
 
-      function removeElement(elementId) {
-          // Removes an element from the document
-          var element = document.getElementById(elementId);
-          element.parentNode.removeChild(element);
+      function removeElement(elementId) { // Removes an element from the document
+        var element = document.getElementById(elementId);
+        element.parentNode.removeChild(element);
       }
     
       })
